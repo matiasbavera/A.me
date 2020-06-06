47c55bc1ad73baf2e88337e258214e8912dbef01
Matias Bavera <matiasbavera@gmail.com>
2020-06-06 00:12:15 -0400

    Added profile picture, background and one project

---
+++ src/portfolio/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Projects.scss';
+
+const Header = () => {
+  return (
+    <div>
+      <div className="profile enhance">
+        <img src="/resources/profile.png" />
+        <h2>Matias Bavera</h2>
+        <p>
+          Full Stack Software Engineer &middot;{' '}
+          <a href="https://www.ekumenlabs.com/">
+            <strong className="ekumen">@Ekumen</strong>
+          </a>{' '}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
